24bf81661d9aa4144f7f33875a1af244bc72b888
Saranya R <93668821+saranyasara19@users.noreply.github.com>
2022-02-15 13:51:43 +0530

    Add files via upload

---
+++ Task1/TodoApp.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import "./todoapp.css";
+
+function TodoApp() {
+  const [task, setTask] = useState("");
+  const [taskList, setTaskList] = useState([]);
+
+  const handleChange = (e) => {
+    setTask(e.target.value);
+  };
+
+  const AddTask = () => {
+    if (task !== "") {
+      const taskDetails = {
+        id: Math.floor(Math.random() * 1000),
+        value: task,
+        isCompleted: false,
+      };
+
+      setTaskList([...taskList, taskDetails]);
+    }
+  };
+
+  const deleteTask = (e, id) => {
+    e.preventDefault();
+    setTaskList(taskList.filter((t) => t.id !== id));
+  };
+
+  const taskCompleted = (e, id) => {
+    e.preventDefault();
+    const element = taskList.findIndex((elem) => elem.id === id);
+
+    const newTaskList = [...taskList];
+
+    newTaskList[element] = {
+      ...newTaskList[element],
+      isCompleted: true,
+    };
+
+    setTaskList(newTaskList);
+  };
+
+  return (
+    <div className="todo">
+      <input
+        type="text"
+        name="text"
+        id="text"
+        onChange={(e) => handleChange(e)}
+        placeholder="Title..."
+      />
+      <button className="add-btn" onClick={AddTask}>
+        Add
+      </button>
+      <br />
+      {taskList !== [] ? (
+        <ul>
+          {taskList.map((t) => (
+            <li className={t.isCompleted ? "crossText" : "listItem"}>
+              {t.value}
+              <button
+                className="completed"
+                onClick={(e) => taskCompleted(e, t.id)}
+              >
+                Completed
+              </button>
+
+              <button className="delete" onClick={(e) => deleteTask(e, t.id)}>
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      ) : null}
+    </div>
+  );
+}
+
+export default TodoApp;
